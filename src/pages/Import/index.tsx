33a157bd2ef31475fd59674cebe1697ca432af3e
jonathancsr <jonathancsrr@gmail.com>
2020-06-07 22:04:54 -0300

    add: import page call api

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
+    uploadedFiles.map(file => {
+      data.append('file', file.file);
+    });
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.map(file => {
+      const fileProps: FileProps = {
+        file,
+        name: file.name,
+        readableSize: `${file.size.toString()} bytes`,
+      };
+
+      setUploadedFiles([...uploadedFiles, fileProps]);
+    });
   }
 
   return (
